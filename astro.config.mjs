a2426d458d98397685ea22df3c0e39a9187b5f6b
Andres Agudelo <andres@MacBook-Pro-de-Andres-Agudelo.local>
2026-02-06 20:25:39 -0500

    feat: add dual adapter support for Vercel and DigitalOcean SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: node({
+  adapter: process.env.VERCEL ? vercel() : node({
     mode: 'standalone',
   }),
 });
